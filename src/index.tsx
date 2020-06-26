c3f1214b8f4a01aa4725f24637ba71d2babe5969
Brooklin Myers <brooklinjazzmyers@gmail.com>
2020-06-25 21:25:14 -0600

    Configure simple countdown

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,22 @@
 import moment from "moment";
-import React, { useState, useRef } from "react";
+import React, { useState, useRef, useEffect } from "react";
 import { Button, Slider, Text, View } from "react-native";
+import { useInterval } from "../hooks/useInterval";
 
 export const PomodoroTimer = () => {
   const [intervalNum, setIntervalNum] = useState(15);
-  const intervalInSeconds = intervalNum * 60 * 1000
-
+  const intervalInMs = intervalNum * 60 * 1000;
+  const [count, setCount] = useState(0)
+  const diff = intervalInMs - count
+  const [started, setStarted] = useState(false)
+  const remainingDuration = moment.duration(diff, "milliseconds")
+  const remainingMilliseconds = remainingDuration.asMilliseconds()
+  const remainingMinutes = moment.utc(remainingMilliseconds).format("mm:ss")
+  useInterval(() => {
+    if (started && remainingMilliseconds !== 0) {
+      setCount(count + 1000)
+    }
+  }, 1000)
   return (
     <View
       style={{
@@ -15,6 +26,7 @@ export const PomodoroTimer = () => {
         width: "100%",
       }}
     >
+      <Text>{remainingMinutes}</Text>
       <Slider
         step={1}
         style={{ width: 200, height: 40 }}
@@ -25,7 +37,7 @@ export const PomodoroTimer = () => {
         minimumTrackTintColor="tomato"
         maximumTrackTintColor="tomato"
       />
-      <Button title={"Start"} onPress={() => true} />
+      <Button title={"Start"} onPress={() => setStarted(true)} />
     </View>
   );
 };
